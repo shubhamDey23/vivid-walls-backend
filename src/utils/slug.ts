16d6e12f90a1cf51757ecdeeef818183b5e67875
shubhamDey23 <111415615+shubhamDey23@users.noreply.github.com>
2026-06-29 20:46:46 +0530

    slug fix

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -1,7 +1,5 @@
 import slugify from "slugify";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "../config/prisma";
 
 /**
  * Convert text to URL slug
